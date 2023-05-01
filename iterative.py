9c5fb645153ccd428ac6db28208b892bf874e164
siregarpetrus56 <siregarpetrus56@gmai.com>
2023-05-02 00:08:04 +0700

    first commit

---
+++ iterative.py
@@ -0,0 +1,7 @@
+def powerOfTwoIt(n):
+    i = 0
+    power = 1
+    while i < n:
+        power = power * 2
+        i = i + 1
+    return power
---
+++ recursion works.py
@@ -0,0 +1,14 @@
+def firstMethod():
+    secondMethod()
+    print ("I am the first Method")
+
+def secondMethod():
+    thirdMethod()
+    print ("I am the second Method")
+
+def thirdMethod():
+    fourthMethod()
+    print ("I am third Method")   
+
+def fourthMethod():
+    print ("I am the fourth Method")
